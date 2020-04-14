246237dcb8dba593dc3198c86b6ef99e26fcd186
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 02:00:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,12 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+#Define a Method that takes in and uses a parameter 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
 
 
 
